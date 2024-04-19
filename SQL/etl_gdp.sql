694fd1e1a5cdb8a99c01965d639d8c93adc09913
Alex Pirlogeanu <alexandru.pirlogeanu@gmail.com>
2024-04-19 08:07:13 +0300

    main

--- SQL/etl_gdp.sql
+++ SQL/etl_gdp.sql
@@ -1,29 +1,48 @@
 -- Table Schema Definition
+																						--| Countries GDP (Main Table)
 CREATE TABLE IF NOT EXISTS etl.countries_gdp (
-    country_gdp_id		SERIAL PRIMARY KEY,
-	country             VARCHAR(50),
-    region              VARCHAR,
-    gdp_usd_billions    FLOAT,
-    period              DATE,
-    created_at          TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    updated_at          TIMESTAMP WITHOUT TIME ZONE DEFAULT NULL,
-	data_hash 			VARCHAR(255),
+    country_gdp_id				SERIAL PRIMARY KEY,
+	country             		VARCHAR(50),
+    region              		VARCHAR,
+    gdp_usd_billions    		FLOAT,
+    period              		DATE,
+    created_at          		TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at          		TIMESTAMP WITHOUT TIME ZONE DEFAULT NULL,
+	data_hash 					VARCHAR(255),
+	batch_id					UUID,
     UNIQUE (country, period)
 );
+																						--| Countries GDP - Update Log (Used to keep tracking changes)
+CREATE TABLE IF NOT EXISTS etl.countries_gdp_update_log (
+	gpd_update_log_id			SERIAL PRIMARY KEY,
+	action_taken				VARCHAR(10),
+	gdp_update_log_timestamp	TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+	country						VARCHAR(50),
+	period						DATE,
+	country_gdp_id				INTEGER,
+	batch_id					UUID,
+	FOREIGN KEY (country_gdp_id)
+		REFERENCES etl.countries_gdp (country_gdp_id)
+		ON DELETE CASCADE
+);
+
 -- Procedure responsible for inserting new records or updating existing ones based on the provided data
 CREATE OR REPLACE PROCEDURE etl.update_gdp_data (
     country_name    VARCHAR,
     gdp_region      VARCHAR,
     gdp_value       FLOAT,
-    gdp_period      DATE
+    gdp_period      DATE,
+	batch_id		UUID
 )
 
 LANGUAGE plpgsql
 AS $$
 
 DECLARE
-	new_hash VARCHAR; -- Variable for the hash of the incoming data
-	existing_record etl.countries_gdp%ROWTYPE; -- Variable to hold existing record data
+	-- Variable for the hash of the incoming data
+	new_hash VARCHAR; 	
+	-- Variable to hold existing record data
+	existing_record etl.countries_gdp%ROWTYPE; 																
 	
 BEGIN
 	-- Calculate a hash of the incoming data to check against existing records
@@ -32,21 +51,32 @@ BEGIN
 	-- Retrieve existing record, if any, based on the country and period
 	SELECT * INTO existing_record
 	FROM etl.countries_gdp
-	WHERE country = country_name and period = gdp_period;
+	WHERE country = country_name AND period = gdp_period;
 	
 	-- If no existing record is found, proceed to insert the new data along with the calculated hash.
 	IF existing_record.country_gdp_id IS NULL THEN
-		insert into etl.countries_gdp(country, region, gdp_usd_billions, period, created_at, data_hash)
-		values(country_name, gdp_region, gdp_value, gdp_period, CURRENT_TIMESTAMP, new_hash);
+		INSERT INTO etl.countries_gdp(country, region, gdp_usd_billions, period, created_at, data_hash, batch_id)
+		VALUES(country_name, gdp_region, gdp_value, gdp_period, CURRENT_TIMESTAMP, new_hash, batch_id)
+		RETURNING country_gdp_id INTO existing_record.country_gdp_id;
+		
+		-- Insertion Log
+		INSERT INTO etl.countries_gdp_update_log(action_taken, country, period, country_gdp_id, batch_id)
+		VALUES ('INSERT', country_name, gdp_period, existing_record.country_gdp_id, batch_id);
 		
 	-- If an existing record is found and the hash differs, update the record with the new data.	
 	ELSEIF new_hash != existing_record.data_hash THEN
-		update etl.countries_gdp
-		set region = gdp_region,
+		UPDATE etl.countries_gdp
+		SET region = gdp_region,
 			gdp_usd_billions = gdp_value,
 			updated_at = CURRENT_TIMESTAMP,
-			data_hash = new_hash
-		where country_gdp_id = existing_record.country_gdp_id;
+			data_hash = new_hash,
+			batch_id = batch_id
+		WHERE country_gdp_id = existing_record.country_gdp_id;
+		
+		-- Updating Log
+		INSERT INTO etl.countries_gdp_update_log(action_taken, country, period, country_gdp_id, batch_id)
+		VALUES ('UPDATE', country_name, gdp_period, existing_record.country_gdp_id, batch_id);
+		
 	END IF;
 	-- No ELSE block is needed as no action is required if the hash matches (no data changes).
 END;
--- SQL/etl_gdp_workings_scripts.sql
+++ SQL/etl_gdp_workings_scripts.sql
@@ -2,6 +2,11 @@ SELECT *
 FROM etl.countries_gdp;
 
 SELECT *
+FROM etl.countries_gdp_update_log;
+
+
+
+SELECT *
 FROM information_schema.columns
 WHERE table_schema = 'etl' and table_name = 'countries_gdp'
 ORDER BY ordinal_position;
@@ -13,6 +18,15 @@ WHERE n.nspname = 'etl' AND proname = 'update_gdp_data';
 
 
 -----------------------------------------------------------------------------------------	Adding New Columns
+																							--| Insert into LOG table FK country_gdp_id
+ALTER TABLE etl.countries_gdp_update_log														-- 1 Inserting Column
+ADD COLUMN country_gdp_id INTEGER;
+
+ALTER TABLE etl.countries_gdp_update_log														-- 2 Add Constraint
+ADD CONSTRAINT fk_countries_gdp
+FOREIGN KEY (country_gdp_id)
+	REFERENCES etl.countries_gdp (country_gdp_id)
+	ON DELETE CASCADE;
 
 																							--| Drop existing composite primary key constraing
 ALTER TABLE etl.countries_gdp 
@@ -20,11 +34,12 @@ DROP CONSTRAINT countries_gdp_pkey;
 																							--| Add the country_gdp_id column as the primary key
 ALTER TABLE etl.countries_gdp
 ADD COLUMN country_gdp_id SERIAL PRIMARY KEY;
-																							--| Add unique constraint for country and period
+
 ALTER TABLE etl.countries_gdp
 ADD CONSTRAINT country_period_unique UNIQUE(country, period);
-
-
-
+																							--| Add the batch_id column as UUID
 ALTER TABLE etl.countries_gdp
-ADD COLUMN country_gdp_id SERIAL PRIMARY KEY;
+ADD COLUMN batch_id UUID;
+																							--| Drop Tables
+DROP TABLE IF EXISTS etl.countries_gdp;
+DROP TABLE IF EXISTS etl.countries_gdp_update_log;
